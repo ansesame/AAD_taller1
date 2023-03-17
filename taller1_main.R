93df0c8013c3a653da281ecf139983475429788b
ansesame <ansesame@gmail.com>
2023-03-17 18:56:10 +0000

    Primer commit desde posit cloud

---
+++ taller1_main.R
@@ -0,0 +1,7 @@
+# Este documento contiene el código requerido para el desarrollo del taller 1
+# del curso de Análisis Avanzado de datos de la maestría MACC en la universidad
+# del Rosario.
+# Código desarrollado por ...
+# Marzo 2023
+################################################################################
+rm(list = ls())
